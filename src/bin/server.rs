d33e9ad8d5ab719af6f93bad8713fd2909a25d6f
Frank Hucek <me@frankhucek.com>
2024-02-08 19:38:57 -0700

    ok, this is fundamentally working, so branching here

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -90,13 +90,15 @@ async fn handle_client(sock: TcpStream, peer_addr: SocketAddr, tls_acceptor: Tls
         println!("Connected via TLS to {}", peer_addr);
 
         let dst = String::new();
-        let mut buf:Vec<u8> = Vec::new();
-        let n = tls_stream.read_buf(&mut buf).await?;
+        // let mut buf:Vec<u8> = Vec::with_capacity(4096);
+        let mut buf = [0;4096];
+        // let n = tls_stream.read_buf(&mut buf).await?;
+        let n = tls_stream.read(&mut buf).await?;
         println!("Read {} bytes from client", n);
         // tls_stream.read(&mut buf).await?;
         // tls_stream.read_to_string(&mut dst).await?;
         
-        let string = String::from_utf8(buf)?;
+        let string = String::from_utf8(buf.to_vec())?;
         println!("Received msg: {}", string);
 
         tls_stream.write_all(string.as_bytes()).await?;
